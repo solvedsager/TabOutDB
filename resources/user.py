83c21058313eb85923235a66281a5d1d63162f37
Calvin <curquhart@att.net>
2019-02-23 11:02:47 -0600

    OG Commit of frame with store items etc

---
+++ resources/user.py
@@ -0,0 +1,31 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type = str,
+        required = True,
+        help = "This Feild cannot be blank"
+    )
+
+    parser.add_argument('password',
+        type = str,
+        required = True,
+        help = "This Feild cannot be blank"
+    )
+
+
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message" : "User Already Exist"}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {"message" : "User created successfully."} , 201
